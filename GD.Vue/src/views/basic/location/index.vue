0e5f542dfd23fdc74cdb228f6b37b27cb7ed570a
LL zhang <zhang31666@outlook.com>
2023-11-17 16:09:06 +0800

    .

---
+++ GD.Vue/src/views/basic/location/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app-container">
+        库位设置
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
---
+++ GD.Vue/src/views/basic/region/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app-container">
+        库区设置
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style></style>
